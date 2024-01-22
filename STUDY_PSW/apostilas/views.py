7111326b4708f6f13a8499e3c79c474cca8d0954
Nobr3c <jnobr3c+github@gmail.com>
2024-01-22 00:32:08 -0300

    :books: Aula 3 | PSW 9.0 - Apostila

--- STUDY_PSW/apostilas/views.py
+++ STUDY_PSW/apostilas/views.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 def adicionar_apostilas(request):
     if request.method == 'GET':
         apostilas = Apostila.objects.filter(user=request.user)
-
+        # TODO: Criar as Tags
         views_totais = ViewApostila.objects.filter(apostila__user = request.user).count()
         return render(request, 'adicionar_apostilas.html', {'apostilas': apostilas, 'views_totais': views_totais})
     elif request.method == 'POST':
@@ -28,3 +28,14 @@ def adicionar_apostilas(request):
             request, constants.SUCCESS, 'Apostila adicionada com sucesso.'
         )
         return redirect('/apostilas/adicionar_apostilas/')
+
+def apostila(request, id):
+    apostila = Apostila.objects.get(id=id)
+    views_unicas = ViewApostila.objects.filter(apostila=apostila).values('ip').distinct().count()
+    views_totais = ViewApostila.objects.filter(apostila=apostila).count()
+    view = ViewApostila(
+        ip=request.META['REMOTE_ADDR'],
+        apostila=apostila
+    )
+    view.save()
+    return render(request, 'apostila.html', {'apostila': apostila, 'views_unicas': views_unicas, 'views_totais': views_totais})
